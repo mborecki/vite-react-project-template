d21c9cf4aa24c7be44921f83f1ae589efa467e73
Marek Borecki <borecki2@gmail.com>
2025-08-12 18:01:57 +0200

    porzadki

--- server/.test-utils/reset-db.ts
+++ server/.test-utils/reset-db.ts
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 export async function resetDB() {
     const collections = (await mongoose.connection.db!.listCollections().toArray()).map(x => x.name);
 
-    console.log('resetDB', collections);
-
     for (var i = 0; i < collections.length; i++) {
         try {
             await mongoose.connection.dropCollection(collections[i]);
--- src/.test-utils/msw.ts
+++ src/.test-utils/msw.ts
@@ -1,7 +1,8 @@
+
 import { setupServer } from 'msw/node'
 
 export const mswServer = setupServer()
 
-mswServer.events.on('request:start', ({ request }) => {
-  console.log('Outgoing request:', request.method, request.url)
-})
+// mswServer.events.on('request:start', ({ request }) => {
+//   console.log('Outgoing request:', request.method, request.url)
+// })
